a5efe170a1e1307a69cd571bd29e8b58a9220542
iBambooFox <30310224+iBambooFox@users.noreply.github.com>
2019-01-30 20:45:07 +0100

    Update CBhop.cpp

--- Aniware/Aniware/Features/CBhop.cpp
+++ Aniware/Aniware/Features/CBhop.cpp
@@ -3,14 +3,8 @@
 namespace Aniware {
 
 	void CBhop::Initialise() {
-
-		if (!g_pCmd or !g_pLoc)
-			return;
-
-		if (g_pCmd->buttons & IN_JUMP and g_pLoc->Flags() > FL_INAIR) {
-
-			g_pCmd->buttons |= IN_JUMP;
-
-		}
+		
+		/* Run Bhop Routine */
+		
 	}
 }
